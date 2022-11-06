a8d9a25defcc83f6ede6962b51cde4de58de1b05
PEPEMAN <raghavwastaken1@gmail.com>
2022-11-06 10:43:17 +0530

    Color Changes and some changes

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
-const navButton = document.querySelector(".nav-button")
-navButton.addEventListener("click", () => {navButton.toggleAttribute('opened')});
+const NAVIGATION = {
+    'navButton': document.querySelector(".nav-button"),
+    'navList' : document.querySelector('.nav-list')
+}
+NAVIGATION.navButton.addEventListener("click", () => {
+    NAVIGATION.navButton.toggleAttribute('opened')
+    NAVIGATION.navButton.innerHTML = NAVIGATION.navButton.hasAttribute('opened')?'close':'menu'
+});
+
+document.addEventListener('click',(e)=>{
+    if (e.target!=NAVIGATION.navButton && NAVIGATION.navButton.hasAttribute("opened") && e.target!=NAVIGATION.navList){
+        NAVIGATION.navButton.toggleAttribute("opened")
+    }
+})
