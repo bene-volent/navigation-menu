e9f6b3345fcd4cbda4be4541874507e89ab82f2a
PEPEMAN <raghavwastaken1@gmail.com>
2022-11-06 11:40:35 +0530

    Bug fixes with Nav Button

--- main.js
+++ main.js
@@ -1,22 +1,27 @@
 const NAVIGATION = {
-    'navButton': document.querySelector(".nav-button"),
-    'navList' : document.querySelector('.nav-list')
-}
-NAVIGATION.navButton.addEventListener("click", () => {
-    NAVIGATION.navButton.toggleAttribute('opened')
-    if (NAVIGATION.navButton.hasAttribute('opened')){
-        NAVIGATION.navButton.innerHTML = 'close'
-        NAVIGATION.navButton.toggleAttribute("aria-hidden",false)
+    navButton: document.querySelector(".nav-button"),
+    navList: document.querySelector(".nav-list"),
+};
+
+function handleNavigationButtonEvent() {
+    NAVIGATION.navButton.toggleAttribute("opened");
+    if (NAVIGATION.navButton.hasAttribute("opened")) {
+        NAVIGATION.navButton.innerHTML = "close";
+        NAVIGATION.navList.setAttribute("aria-hidden", false);
+    } else {
+        NAVIGATION.navButton.innerHTML = "menu";
+        NAVIGATION.navList.setAttribute("aria-hidden", true);
     }
-    else{
-            NAVIGATION.navButton.innerHTML = 'menu'
-            NAVIGATION.navButton.toggleAttribute("aria-hidden",true)
+}
+
+NAVIGATION.navButton.addEventListener("click", handleNavigationButtonEvent);
+
+document.addEventListener("click", (e) => {
+    if (
+        e.target != NAVIGATION.navButton &&
+        NAVIGATION.navButton.hasAttribute("opened") &&
+        e.target != NAVIGATION.navList
+    ) {
+        handleNavigationButtonEvent();
     }
 });
-
-document.addEventListener('click',(e)=>{
-    
-    if (e.target!=NAVIGATION.navButton && NAVIGATION.navButton.hasAttribute("opened") && (e.target!=NAVIGATION.navList )){
-        NAVIGATION.navButton.toggleAttribute("opened")
-    }
-})
